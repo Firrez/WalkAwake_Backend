cb5a43b6d8aea8badac5c254f6a4d0ff9409956f
firrez <fakima@frnet.dk>
2023-12-06 12:51:41 +0100

    Minor additions

--- AlarmModule/AlarmManager.cpp
+++ AlarmModule/AlarmManager.cpp
@@ -1,11 +1,31 @@
 #include "AlarmManager.hpp"
 
 using namespace AlarmModule;
+using namespace CameraModule;
 
-AlarmManager::AlarmManager() = default;
+AlarmManager::AlarmManager() {
+    cameraManager = CameraManager();
+}
 
 int AlarmManager::UpdateAlarms(string m_strAlarms) {
 
     return 0;
+}
+
+string AlarmManager::GetAlarms() {
+    return std::string();
+}
+
+int AlarmManager::TriggerCallback() {
+    return 0;
+}
+
+void AlarmManager::SetTrigger() {
 
 }
+
+void AlarmManager::ActivateAlarm() {
+
+}
+
+AlarmManager::~AlarmManager() = default;
---
+++ AlarmModule/SoundController.cpp
@@ -0,0 +1,8 @@
+//
+// Created by firrez on 12/6/23.
+//
+
+#include "SoundController.hpp"
+
+namespace AlarmModule {
+} // AlarmModule
