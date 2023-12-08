a879cbf8c391dc7f2ecd64527bef312e8b544f5c
firrez <fakima@frnet.dk>
2023-12-08 10:31:54 +0100

    AlarmTrigger.cpp done + cmake updated

--- AlarmModule/AlarmTrigger.hpp
+++ AlarmModule/AlarmTrigger.hpp
@@ -1,16 +1,23 @@
 #pragma once
 
 #include <functional>
+#include <string>
+
+using namespace std;
 
 namespace AlarmModule {
     class AlarmTrigger {
     public:
         AlarmTrigger();
-        int SetAlarm();
-        int RegisterCallback(std::function<void> CallBack);
+        int SetAlarm(const string& m_strNextAlarm);
+        int StopAlarm();
+        int RegisterCallback(function<void()> CallBack);
         virtual ~AlarmTrigger();
 
     private:
-
+        pthread_t m_pthAlarmThread{};
+        static bool IsActive;
+        static function<void()> m_ptrCallBack;
+        static void *ClockTimer(void *arg1);
     };
 }
