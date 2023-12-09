5eb446a19b06c2b1d40289a882a1dd47f4df23b4
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-12-09 15:30:52 +0100

    Update WA_binder.cpp
    Added camera module to Py bind11

--- WA_binder.cpp
+++ WA_binder.cpp
@@ -1,2 +1,18 @@
 #include <pybind11/pybind11.h>
+#include "CameraModule/ComputerVisionManager.hpp"
+using namespace CameraModule;
+namespace py = pybind11;
 
+PYBIND11_MODULE(WalkAwake, m){
+    py::class_<ComputerVisionManager>(m, "ComputerVisionManager")
+            .def(py::init<>())
+
+            .def("VerifyImage", &ComputerVisionManager::VerifyImage,
+                 "Target: Find target, ImagePath: path of image to detect",
+                 py::arg("target"),py::arg("image_path"))
+
+            .def("Start",&ComputerVisionManager::Start,
+                 "Start the camera live feed", py::arg("image_feed_path"))
+
+            .def("Stop", &ComputerVisionManager::Stop);
+}
