d09755e1eea78be9688a73cb2660c465bff87272
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2023-12-09 15:53:14 +0100

    Update WA_binder.cpp
    added binding for CV manager and alarm manager

---
+++ WA_binder.cpp
@@ -0,0 +1,34 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+
+#include "CameraModule/ComputerVisionManager.hpp"
+#include "AlarmModule/AlarmManager.hpp"
+
+using namespace CameraModule;
+using namespace AlarmModule;
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(WalkAwake, m){
+    py::class_<ComputerVisionManager>(m, "ComputerVisionManager")
+            .def(py::init<>())
+
+            .def("VerifyImage", &ComputerVisionManager::VerifyImage,
+                 "Target: Find target, ImagePath: path of image to detect",
+                 py::arg("target"),py::arg("image_path"))
+
+            .def("Start",&ComputerVisionManager::Start,
+                 "Start the camera live feed", py::arg("image_feed_path"))
+
+            .def("Stop", &ComputerVisionManager::Stop);
+
+    py::class_<AlarmManager>(m, "AlarmManager")
+            .def(py::init<const std::function<void()> &>())
+
+            .def("UpdateAlarms",&AlarmManager::UpdateAlarms,
+                 "Update alarms from json", py::arg("alarms_json"))
+
+            .def("GetAlarms", &AlarmManager::GetAlarms);
+
+}
